5454db4cbd8662ee983547497a895efc0edb8b72
rjlacanlaled <rjlacanlaled@gmail.com>
2023-11-05 12:49:42 +0800

    restructure project

---
+++ src/arrays/mod.rs
@@ -0,0 +1,6 @@
+pub mod contains_duplicate;
+pub mod product_except_self;
+pub mod max_sub_array;
+pub mod max_profit;
+pub mod max_product_sub_array;
+pub mod min_rotated_sorted_array;
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-// sort
-mod insertion_sort;
-mod merge_sort;
-mod quick_sort;
-
 // array
-mod contains_duplicate;
-mod product_except_self;
-mod max_sub_array;
-mod max_profit;
-mod max_product_sub_array;
+mod arrays;
+mod sorting;
 
 fn main() {
     let mut arr = [2, -5, -2, -4, 3];
@@ -22,5 +14,5 @@ fn main() {
     // quick_sort::quick_sort(&mut arr);
     // println!("quick_sort {:?}", arr)
 
-    max_product_sub_array::max_product(arr.to_vec());
+    arrays::max_product_sub_array::max_product(arr.to_vec());
 }

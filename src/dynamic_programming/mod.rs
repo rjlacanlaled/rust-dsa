1f31188b8d6019f87b4a1eb9a3fd66c5e114f476
rjlacanlaled <rjlacanlaled@gmail.com>
2023-11-06 17:48:26 +0800

    practicing sliding window

--- src/dynamic_programming/mod.rs
+++ src/dynamic_programming/mod.rs
@@ -1,3 +1,4 @@
 pub mod climbing_stairs;
 pub mod coin_change;
 pub mod longest_increasing_subsequence;
+pub mod longest_common_subsequence;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 // array
 mod arrays;
 mod sorting;
@@ -5,31 +7,54 @@ mod binary;
 mod dynamic_programming;
 
 fn main() {
-    let mut arr = [0, 0, 0, 1, 10, 100, -2, -10, 5, 5, 10, 100];
+    let nums = [1, 2, 3, 1];
 
-    // insertion_sort::insertion_sort(&mut arr);
-    // println!("insertion_sort {:?}", arr);
-
-    // merge_sort::merge_sort(&mut arr);
-    // println!("merge_sort {:?}", arr);
-
-    // quick_sort::quick_sort(&mut arr);
-    // println!("quick_sort {:?}", arr)
-
-    // let min = arrays::min_rotated_sorted_array::find_min(arr.to_vec());
-    // println!("min {:?}", min);
-
-    // let target = arrays::three_sum::three_sum(arr.to_vec());
-    // println!("target {:?}", target);
-
-    // let sum = arrays::container_with_most_water::max_area(arr.to_vec());
-    // println!("sum {:?}", sum);
-
-    // let sum = arrays::three_sum_closest::three_sum_closest(arr.to_vec(), 199);
-    // println!("sum {:?}", sum)
-
-    let steps = dynamic_programming::longest_increasing_subsequence::length_of_lis(
-        vec![10, 9, 2, 5, 3, 7, 101, 18]
-    );
-    println!("steps {:?}", steps);
+    let max_average = find_max_average(vec![1, 12, -5, -6, 50, 3], 4);
+    println!("{}", max_average);
 }
+
+pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+    let mut visited: HashMap<i32, i32> = HashMap::new();
+
+    for i in 0..nums.len() {
+        let duplicate = visited.get(&nums[i]);
+        match duplicate {
+            Some(v) => {
+                if (v - (i as i32)).abs() <= k {
+                    return true;
+                } else {
+                    visited.insert(nums[i], i as i32);
+                }
+            }
+            None => {
+                visited.insert(nums[i], i as i32);
+            }
+        }
+    }
+
+    false
+}
+
+pub fn find_max_average(nums: Vec<i32>, k: i32) -> f64 {
+    let mut left = 0;
+    let mut right = k as usize;
+    let mut current_sum = 0.0;
+
+    for i in left..=right - 1 {
+        current_sum += nums[i] as f64;
+    }
+
+    let mut max_average = current_sum / (k as f64);
+
+    while right < nums.len() {
+        current_sum -= nums[left] as f64;
+        current_sum += nums[right] as f64;
+        max_average = max_average.max(current_sum / (k as f64));
+        left += 1;
+        right += 1;
+    }
+
+    max_average
+}
+
+pub fn longest_nice_substring(s: String) -> String {}
